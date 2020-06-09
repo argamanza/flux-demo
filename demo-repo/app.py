422c3cba638a5b308db0725d25c0b66f721f0fc8
Tzahi Argaman <argamanza@gmail.com>
2020-06-10 00:46:17 +0300

    fix test syntax

--- demo-repo/app.py
+++ demo-repo/app.py
@@ -7,13 +7,6 @@ app = Flask(__name__)
 def hello():
     return "Flask inside Docker!!"
 
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    assert func(4) == 5
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
