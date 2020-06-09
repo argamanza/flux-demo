4c515502ab6524ef0094daab70f991ba9dc87a1e
Tzahi Argaman <argamanza@gmail.com>
2020-06-10 00:44:01 +0300

    add test

--- demo-repo/app.py
+++ demo-repo/app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def hello():
     return "Flask inside Docker!!"
 
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(4) == 5
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
